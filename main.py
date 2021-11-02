a3e5a7ba96519b6d09240c5bab621fb3a2673f0e
Shubham <shubham3357@outlook.com>
2021-11-02 18:25:56 +0530

    Inbuilt calling in StringPairs->string_pairs

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ import StringClass
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    name = "123"
-    print(name.split())
-    for i in name:
-        print(i, end=" ")
 
+    n = 5
+
+    for i in range(1, n + 1):
+
+        # first element is always 1
+        C = 1
+        for j in range(1, i + 1):
+            # first value in a line is always 1
+            print(' ', C, end='')
+
+            # using Binomial Coefficient
+            C = C * (i - j) // j
+
+        for j in range(0, n - i):
+            print('  0', end='')
+        print()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
